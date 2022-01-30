9b25d9ac1a70e780c73e509de996d886c6ab98bc
Somesh Ranjan <77766761+somesh202@users.noreply.github.com>
2022-01-30 21:47:49 +0530

    Update CartCard_2.jsx

--- src/components/CartCard_2.jsx
+++ src/components/CartCard_2.jsx
@@ -42,7 +42,7 @@ function CartCard_2(props) {
       userID: userID,
       itemId: mongooseId
     };
-    const res = await fetch('api/cart', {
+    const res = await fetch('https://fmcw-deployedp.herokuapp.com/api/cart', {
       method: 'DELETE',
       body: JSON.stringify(obj),
       headers: {
