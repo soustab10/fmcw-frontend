cb491bc94faa889a7a9d4d830a3021bfe11e6c41
Somesh Ranjan <77766761+somesh202@users.noreply.github.com>
2022-01-30 21:13:25 +0530

    Update Dashboard.jsx

--- src/components/pages/Dashboard/Dashboard.jsx
+++ src/components/pages/Dashboard/Dashboard.jsx
@@ -36,7 +36,7 @@ function Dashboard() {
       setIsLoading(true);
       const token = sessionStorage.getItem('tokenID');
       try {
-        const res = await fetch('/api/user', {
+        const res = await fetch('https://fmcw-deployedp.herokuapp.com/api/user', {
           method: 'GET',
           // body: JSON.stringify({
           //   token: token
