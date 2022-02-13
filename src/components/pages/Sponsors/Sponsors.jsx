e7c3e2e397f6412507e91dc7aa211218f1272004
somesh202 <77766761+somesh202@users.noreply.github.com>
2022-02-13 15:17:04 +0530

    Re-ordered sponsors

--- src/components/pages/Sponsors/Sponsors.jsx
+++ src/components/pages/Sponsors/Sponsors.jsx
@@ -68,17 +68,31 @@ function Sponsors(props) {
         <div className="cards-sponsor">
           <SponsorCard
             className="sponsor-card"
-            img="https://drive.google.com/uc?export=view&id=1g24NVNEBcteGSJTCjQxU2q4rQW37ouWX"
-            name="National Media Partner"
-            position="Amar Ujala"
-            websiteLink="https://www.amarujala.com/"
+            img="https://drive.google.com/uc?export=view&id=1u2jXRFm5Nm3sGOxJmdIj-C9yPJxSXhj5"
+            name="Design partner"
+            position="Huion"
+            websiteLink="https://www.huion.com/"
           />
           <SponsorCard
             className="sponsor-card"
-            img="https://drive.google.com/uc?export=view&id=1g5MXjiel7FwzJUTaGGhbLQtc7f--2XKi"
-            name="Learning Partner"
-            position="Geeks For Geeks"
-            websiteLink="https://www.geeksforgeeks.org/"
+            img="https://drive.google.com/uc?export=view&id=1hCKPGMn4BeDqc8kmtVWy6j_ToPdRFAjp"
+            name="Audio Electronics Partner"
+            position="Urbanista"
+            websiteLink="https://www.urbanista.com/gl/"
+          />
+          <SponsorCard
+            className="sponsor-card"
+            img="/ims.png"
+            name="MBA Prep Partner"
+            position="IMS"
+            websiteLink="https://www.imsindia.com/"
+          />
+          <SponsorCard
+            className="sponsor-card"
+            img="https://drive.google.com/uc?export=view&id=1mH6JcC1poom7Yi7NaJLBqwuGTr3P265u"
+            name="Dating Partner"
+            position="NyuCrush"
+            websiteLink="https://www.nyucrush.com/"
           />
           <SponsorCard
             className="sponsor-card"
@@ -89,6 +103,13 @@ function Sponsors(props) {
           />
           <SponsorCard
             className="sponsor-card"
+            img="https://drive.google.com/uc?export=view&id=1g5MXjiel7FwzJUTaGGhbLQtc7f--2XKi"
+            name="Learning Partner"
+            position="Geeks For Geeks"
+            websiteLink="https://www.geeksforgeeks.org/"
+          />
+          <SponsorCard
+            className="sponsor-card"
             img="https://drive.google.com/uc?export=view&id=11shE6eN5q8k_a3lC-CknFSyTETHX5Q0z"
             name="Book Reading Partner"
             position="Billionbooks"
@@ -96,10 +117,17 @@ function Sponsors(props) {
           />
           <SponsorCard
             className="sponsor-card"
-            img="https://drive.google.com/uc?export=view&id=1u2jXRFm5Nm3sGOxJmdIj-C9yPJxSXhj5"
-            name="Design partner"
-            position="Huion"
-            websiteLink="https://www.huion.com/"
+            img="https://drive.google.com/uc?export=view&id=1Osvu9_SgRHSEC_zSw2Vm0Ruetug7e6eb"
+            name="Merch Partner"
+            position="Festee"
+            websiteLink="https://www.instagram.com/festee_official/?hl=en"
+          />
+          <SponsorCard
+            className="sponsor-card"
+            img="https://drive.google.com/uc?export=view&id=1g24NVNEBcteGSJTCjQxU2q4rQW37ouWX"
+            name="National Media Partner"
+            position="Amar Ujala"
+            websiteLink="https://www.amarujala.com/"
           />
           <SponsorCard
             className="sponsor-card"
@@ -108,80 +136,7 @@ function Sponsors(props) {
             position="India Education"
             websiteLink="https://www.indiaeducation.net/"
           />
-          <SponsorCard
-            className="sponsor-card"
-            img="/dating_partner.png"
-            name="Dating Partner"
-            position="NyuCrush"
-            websiteLink="https://www.nyucrush.com/"
-          />
-          <SponsorCard
-            className="sponsor-card"
-            img="https://drive.google.com/uc?export=view&id=1hCKPGMn4BeDqc8kmtVWy6j_ToPdRFAjp"
-            name="Audio Electronics Partner"
-            position="Urbanista"
-            websiteLink="https://www.urbanista.com/gl/"
-          />
         </div>
-
-        {/* <div className="separator-1">
-          
-          <div className="line" />
-          <h2>Previous Sponsors</h2>
-          <div className="line" />
-        </div>
-        <div className="previous-sponsors">
-        <PrevSponsorCard
-            className="sponsor-card"
-            img="https://drive.google.com/uc?export=view&id=1sW-Sn1Sf7p1b1Y7fuyzny6oBTcMYNA0i"
-            name="Giottus"
-            position="Giottus Technologies"
-          />
-        <PrevSponsorCard
-            className="sponsor-card"
-            img="https://drive.google.com/uc?export=view&id=1sW-Sn1Sf7p1b1Y7fuyzny6oBTcMYNA0i"
-            name="Giottus"
-            position="Giottus Technologies"
-          />
-        <PrevSponsorCard
-            className="sponsor-card"
-            img="https://drive.google.com/uc?export=view&id=1sW-Sn1Sf7p1b1Y7fuyzny6oBTcMYNA0i"
-            name="Giottus"
-            position="Giottus Technologies"
-          />
-        <PrevSponsorCard
-            className="sponsor-card"
-            img="https://drive.google.com/uc?export=view&id=1sW-Sn1Sf7p1b1Y7fuyzny6oBTcMYNA0i"
-            name="Giottus"
-            position="Giottus Technologies"
-          /> */}
-        {/* <img
-            src="https://drive.google.com/uc?export=view&id=12A29XV2fD4rGtifwJopcc2GFFL6WcUWl"
-            alt=""
-          />
-          <img
-            src="https://drive.google.com/uc?export=view&id=1EOWn_3PXnOh1sEPe9TlRuqFCK7K-GsvF"
-            alt=""
-          />
-          <img
-            src="https://drive.google.com/uc?export=view&id=1XIyjF9ylgxpiz9I9YatfjD0jrZN4UTcN"
-            alt=""
-          />
-          <img
-            src="https://drive.google.com/uc?export=view&id=16DGS49dzcLSIgB_0CDIacJoVVH8X_bNR"
-            alt=""
-          />
-          <img
-            src="https://drive.google.com/uc?export=view&id=16L6DxNPN3LLePjwuC6Kj_OJ1Jz-XhMNV"
-            alt=""
-          />
-          <img src="https://drive.google.com/uc?export=view&id=" alt="" />
-          <img src="https://drive.google.com/uc?export=view&id=" alt="" />
-          <img src="https://drive.google.com/uc?export=view&id=" alt="" />
-          <img src="https://drive.google.com/uc?export=view&id=" alt="" />
-          <img src="https://drive.google.com/uc?export=view&id=" alt="" />
-          <img src="https://drive.google.com/uc?export=view&id=" alt="" />
-          <img src="https://drive.google.com/uc?export=view&id=" alt="" /> */}
       </div>
       <div className="sponsors_footer">
         <Footer />
