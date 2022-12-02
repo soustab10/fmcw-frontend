f9e3cee4e5232111b9708569a16504b602c397c5
Lalit singh <99964550+9389lalit@users.noreply.github.com>
2022-12-02 18:31:24 +0530

    Update EventCard.js

--- src/components/pages/Events/EventCard.js
+++ src/components/pages/Events/EventCard.js
@@ -1,16 +1,9 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from 'react';
-// import Card from '@mui/material/Card';
-// import CardContent from '@mui/material/CardContent';
-// import CardMedia from '@mui/material/CardMedia';
-// import Typography from '@mui/material/Typography';
 import './events.css';
-// import { Button } from '@mui/material';
-// import unicorn from "./assets/test.png";
 import styled from 'styled-components';
 import Tilt from 'react-tilt';
-// import addToCart from './CartModal';
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useCart } from 'react-use-cart';
 
@@ -25,37 +18,13 @@ function EventCard(props) {
   const [buttonText, setButtonText] = useState('Next');
   const { addItem, inCart } = useCart();
   const { getItem } = useCart();
-  const handleClick = () => {
-    setClick(!click);
-  };
-  function change() {
-    var elem = document.querySelector('.cart-btn');
-    if (elem.value == 'Add') {
-      elem.value = 'Added';
-    } else {
-      elem.value = 'Add';
-    }
-  }
 
   async function addItemToCart(item) {
-    // console.log('yash');
     const userID = sessionStorage.getItem('userID');
-    // e.preventDefault();
     let obj = {
       userID: userID,
       cartItem: item
     };
-
-    const res = await fetch(process.env.REACT_APP_BACKEND_URI + '/api/cart', {
-      method: 'POST',
-      body: JSON.stringify(obj),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    // console.log({ obj });
-
-    // alert('Item added to cart successfully!');/
     window.setTimeout(function () {
       location.reload();
     }, 1000);
@@ -65,11 +34,6 @@ function EventCard(props) {
       draggable: true,
       icon: false
     });
-
-    // post not working because status isn't upadated in data.js;
-
-    //only for show purposed
-    // window.location.href = "/register";
   }
   const options = {
     reverse: true,
@@ -88,12 +52,8 @@ function EventCard(props) {
           <button
             className="cart-btn"
             onClick={() => {
-              // send post request to database
 
               addItemToCart(props.item);
-              // addItem(props.item);
-
-              // change();
             }}>
             Add <img src="/add-cartPURPLE_OLD_1.svg" />
           </button>
