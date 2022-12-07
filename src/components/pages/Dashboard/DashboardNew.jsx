844827ba890155b7500f5eaefc65760844173239
Somesh Ranjan <77766761+somesh202@users.noreply.github.com>
2022-12-07 12:25:18 +0530

    Revert "Revert "Revealing navigations (#32)" (#33)"
    This reverts commit 58303ee63cb3061dc3ba22f0d77bafd77e412108.

--- src/components/pages/Dashboard/DashboardNew.jsx
+++ src/components/pages/Dashboard/DashboardNew.jsx
@@ -1,13 +1,16 @@
 /* eslint-disable react/react-in-jsx-scope */
-import { useState, useEffect } from 'react';
+import Loading from '../../Loading';
+import { useState, useEffect, useContext } from 'react';
 import Classes from './DashboardNew.module.css';
 import EventCard from './EventCard';
 import WorkshopCard from './WorkshopCard';
 import { useCart } from 'react-use-cart';
 import Button from './Button/Button';
 import Footer from '../../Footer';
+// import './Events.css';
 
 function DashBoardNew() {
+  // const authCtx = useContext()
   const { items } = useCart();
   const [isLoading, setIsLoading] = useState(false);
   const [userData, setUserData] = useState({
@@ -23,6 +26,7 @@ function DashBoardNew() {
   });
   const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
   const logoutHandler = () => {
+    // console.log("yash");
     sessionStorage.clear();
     window.location.href = '/';
     closeMobileMenu();
@@ -34,29 +38,51 @@ function DashBoardNew() {
       try {
         const res = await fetch(process.env.REACT_APP_BACKEND_URI + '/api/user', {
           method: 'GET',
+          // body: JSON.stringify({
+          //   token: token
+          // }),
           headers: {
             'Content-Type': 'application/json',
             token: token
           }
         });
         const data = await res.json();
+        // console.log(data);
+
+        // data has message : 'success' if valid and 'invalid' else
+        // on valid, data also has user.email, user.name, user.isNewUser, user.role
         if (data.message === 'success') {
           localStorage.removeItem('react-use-cart');
           console.log(data);
+          // console.log(data.user.userID.userCart.cartItems);
           if (data.user.userID) {
-            setUserData((_prevState) => ({
+            setUserData((prevState) => ({
+              // ...prevState,
               name: data.user.userID.name,
+              // email: data.user.userID.email,
+              // college: data.user.userID.college,
+              // phone: data.user.userID.number,
+              // year: data.user.userID.yearOfStudy,
+              // instaHandle: data.user.userID.instaHandle,
               userType: data.user.userID.role,
               refCode: data.user.ref_code,
+              // timesReferred: data.user.norefcode,
               cartItems: data.user.userID.userCart.cartItems
             }));
             sessionStorage.setItem('userID', data.user.userID._id);
           } else {
             setUserData((prevState) => ({
+              // ...prevState,
               name: data.user.name,
+              // email: data.user.email,
+              // college: data.user.college,
+              // phone: data.user.number,
+              // year: data.user.yearOfStudy,
+              // instaHandle: data.user.instaHandle,
               userType: data.user.role,
               cartItems: data.user.userCart.cartItems,
               refCode: data.user.ref_code
+              // timesReferred:
             }));
             sessionStorage.setItem('userID', data.user._id);
           }
@@ -72,6 +98,9 @@ function DashBoardNew() {
       setIsLoading(false);
     };
     isTokenValid();
+    // window.location.reload();
+
+    // console.log(isTokenValid());
   }, []);
 
   return (
@@ -92,6 +121,7 @@ function DashBoardNew() {
         </div>
       </div>
       <div className={Classes.contest_cards}>
+        {/* <div className="event-cards"> */}
         <div className={Classes.event_cards}>
           {userData.cartItems.map((item, index) => {
             if (item.Type === 'Contest') {
@@ -116,6 +146,7 @@ function DashBoardNew() {
         <h2>Registered Workshops</h2>
       </div>
       <div className={Classes.contest_cards}>
+        {/* <div className="event-cards"> */}
         <div className={Classes.event_cards}>
           {userData.cartItems.map((item, index) => {
             if (item.Type === 'Workshop') {
@@ -138,7 +169,14 @@ function DashBoardNew() {
         </div>
       </div>
       <div className={Classes.logout_button}>
-        <Button onClick={logoutHandler}>LOG OUT</Button>
+        <Button
+          // isInternalLink
+          // toLink="/"
+          // buttonStyle="btn--primary"
+          // className="nav-links sign"
+          onClick={logoutHandler}>
+          LOG OUT
+        </Button>
       </div>
       <Footer />
     </div>
--- src/store/test-context.jsx
+++ src/store/test-context.jsx
@@ -1,11 +1,10 @@
-/* eslint-disable react/prop-types */
 import React, { useState } from 'react';
 
 const AuthContext = React.createContext({
   token: '',
   isLoggedIn: false,
   login: (token) => {},
-  logout: () => {}
+  logout: () => {},
 });
 
 export const AuthContextProvider = (props) => {
@@ -25,10 +24,14 @@ export const AuthContextProvider = (props) => {
     token: token,
     isLoggedIn: userIsLoggedIn,
     login: loginHandler,
-    logout: logoutHandler
+    logout: logoutHandler,
   };
 
-  return <AuthContext.Provider value={contextValue}>{props.children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthContext;
