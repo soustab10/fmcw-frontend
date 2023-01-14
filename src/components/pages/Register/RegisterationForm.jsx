1c9ea13365a481535028d8ae8652aecb9ee21f75
AmitSharma512 <110725065+AmitSharma512@users.noreply.github.com>
2023-01-14 16:07:52 +0530

    Created Register Page and fixed its responsiveness (#69)
    * Added Register Page
    
    * Added Register Page
    
    * Fixed responsive

--- src/components/pages/Register/RegisterationForm.jsx
+++ src/components/pages/Register/RegisterationForm.jsx
@@ -1,6 +1,8 @@
 import { Fragment, useContext } from 'react';
 import AuthContext from '../../../store/auth-context';
 import Classes from './Registeration.module.css';
+import star from './Star.png';
+import cube from './Cube.svg';
 function RegisterationForm() {
   async function handleSubmit(e) {
     e.preventDefault();
@@ -34,122 +36,89 @@ function RegisterationForm() {
   }
   const authCtx = useContext(AuthContext);
   return (
-    <div className={Classes.section}>
-      <form
-        className={`${Classes.section} ${Classes.form_class}`}
-        onSubmit={(e) => handleSubmit(e)}>
-        <div className={Classes.container}>
-          <label htmlFor="name">
-            <b>Name</b>
-          </label>
-          <input type="text" name="name" value={sessionStorage.getItem('name')} required readOnly />
-          <label htmlFor="email">
-            <b>E-mail</b>
-          </label>
+    <form className={`${Classes.section} ${Classes.form_class}`} onSubmit={(e) => handleSubmit(e)}>
+      <div className={Classes.container}>
+        <img src={star} className={Classes.star} />
+        <input
+          type="text"
+          name="name"
+          value={sessionStorage.getItem('name')}
+          required
+          readOnly
+          hidden
+        />
+        <input
+          type="email"
+          name="email"
+          value={sessionStorage.getItem('email')}
+          required
+          readOnly
+          hidden
+        />
+        <label htmlFor="college" className={Classes.title}>
+          <b>University / College Name</b>
+        </label>
+        {sessionStorage.getItem('email').endsWith('@iitbhu.ac.in') ||
+        sessionStorage.getItem('email').endsWith('@itbhu.ac.in') ? (
           <input
-            type="email"
-            name="email"
-            value={sessionStorage.getItem('email')}
+            type="text"
+            name="college"
+            value="Indian Institute of Technology (BHU) Varanasi"
             required
             readOnly
           />
-          <label htmlFor="college">
-            <b>University / College</b>
-          </label>
+        ) : (
+          <input
+            type="text"
+            name="college"
+            placeholder="e.g. - Indian Institute of Technology (BHU) Varanasi"
+            required
+          />
+        )}
+        <label htmlFor="phone" className={Classes.title}>
+          <b>Phone Number (+91)</b>
+        </label>
+        <input type="tel" name="phone" required />
+        <label htmlFor="year" className={Classes.title}>
+          <b>Year of Study</b>
+        </label>
+        <select name="year">
+          <option value="1">I</option>
+          <option value="2">II</option>
+          <option value="3">III</option>
+          <option value="4">IV</option>
+          <option value="5">V</option>
+        </select>
+        <label htmlFor="redeem" className={Classes.title}>
+          <b>Referral Code</b>
+        </label>
+        <input type="text" name="redeem" placeholder="XXXXXX" />
+        <label htmlFor="insta" className={Classes.title}>
+          <b>Instagram Handle</b>
+        </label>
+        <input type="text" name="insta" placeholder="" required />
+        <label htmlFor="position" className={Classes.title}>
+          <b>Wanna be a?</b>
+        </label>
+        <select name="position">
           {sessionStorage.getItem('email').endsWith('@iitbhu.ac.in') ||
           sessionStorage.getItem('email').endsWith('@itbhu.ac.in') ? (
-            <input
-              type="text"
-              name="college"
-              value="Indian Institute of Technology (BHU) Varanasi"
-              required
-              readOnly
-            />
+            <option value="0">Participant</option>
           ) : (
-            <input
-              type="text"
-              name="college"
-              placeholder="e.g. - Indian Institute of Technology (BHU) Varanasi"
-              required
-            />
+            <Fragment>
+              <option value="1">Participant</option>
+              <option value="2">Campus Ambassador</option>
+            </Fragment>
           )}
-
-          <label htmlFor="phone">
-            <b>Phone Number (+91)</b>
-          </label>
-          <input type="tel" name="phone" required />
-          <label htmlFor="year">
-            <b>Year of Study</b>
-          </label>
-          <select name="year">
-            <option value="1">I</option>
-            <option value="2">II</option>
-            <option value="3">III</option>
-            <option value="4">IV</option>
-            <option value="5">V</option>
-          </select>
-          <label htmlFor="redeem">
-            <b>Referral Code</b>
-          </label>
-          <input type="text" name="redeem" placeholder="XXXXXX" />
-          <label htmlFor="insta">
-            <b>Instagram Handle</b>
-          </label>
-          <input type="text" name="insta" placeholder="" required />
-          <label htmlFor="position">
-            <b>Wanna be a?</b>
-          </label>
-          <select name="position">
-            {sessionStorage.getItem('email').endsWith('@iitbhu.ac.in') ||
-            sessionStorage.getItem('email').endsWith('@itbhu.ac.in') ? (
-              <option value="0">Participant</option>
-            ) : (
-              <Fragment>
-                <option value="1">Participant</option>
-                <option value="2">Campus Ambassador</option>
-              </Fragment>
-            )}
-          </select>
-          <div className={Classes.button_div}>
-            <button type="submit">Submit</button>
-          </div>
+        </select>
+        <div className={Classes.button_div}>
+          <button className={Classes.submit} type="submit">
+            Submit
+          </button>
         </div>
-      </form>
-    </div>
-    //     <form>
-
-    //     <div className={Classes.container}>
-    //       <h1>Sign Up</h1>
-    //       <p>Please fill in this form to create an account.</p>
-
-    //           <label for="email"><b>Username</b></label>
-    //         <input type="text" name="username" placeholder="Enter Username" required />
-    //       <label for="email"><b>Email</b></label>
-    //       <input type="text" placeholder="Enter Email" name="email" required />
-
-    //       <label for="psw"><b>Password</b></label>
-    //       <input type="password" placeholder="Enter Password" name="psw" required />
-
-    //               <label for="email"><b>Phone Number</b></label>
-    //         <br/>
-    //        <select name="phoneCode" required>
-    //         <option selected hidden value="">Code</option>
-    //         <option value="66">+98</option>
-    //         <option value="66">+99</option>
-    //         <option value="66">+90</option>
-    //         <option value="66">+66</option>
-    //        </select>
-    //        <input type="phone" name="phone" placeholder="812345678" required />
-
-    //       <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
-
-    //       <div class="clearfix">
-
-    //         <button type="submit" class="btn">Sign Up</button>
-    //       </div>
-    //     </div>
-
-    //   </form>
+        <img src={cube} className={Classes.cube} />
+      </div>
+    </form>
   );
 }
 
