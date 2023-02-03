a00ff3aa14013094bdb28d7959e723b2cf05b5f1
Amit Sharma <amit.sharma.mec21@itbhu.ac.in>
2023-02-03 20:51:22 +0530

    Updates previous sponsor section , sponsors design

--- src/components/pages/Sponsors/Sponsors1.jsx
+++ src/components/pages/Sponsors/Sponsors1.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Card from './SponsorSection/SponsorsCard.jsx';
 import SponsorData from './SponsorData1.js';
+import SponsorsData from './SponsorsData.js';
 import styled from 'styled-components';
 import { Fade } from 'react-reveal';
 import leftStar from '../../leftStar.png';
 import rightStar from '../../rightStar.png';
 import Footer from '../../Footer';
-import Classes from "./Sponsors1.module.css"
+import Classes from './Sponsors1.module.css';
 
 const Heading = styled.div`
   position: relative;
@@ -18,7 +19,7 @@ const Heading = styled.div`
   border: 3px solid #000000;
   display: flex;
   justify-content: center;
-  h1{
+  h1 {
     padding: 15px;
     font-size: 60px;
     text-align: center;
@@ -36,19 +37,19 @@ const Heading = styled.div`
     position: relative;
     top: 30%;
   }
-  span{
-    top : 13%;
+  span {
+    top: 13%;
   }
   @media (max-width: 768px) {
-      height: 15vh;
-      span {
-        font-size: 50px;
-        text-shadow: 3px 4px 0px #000000;
-      }
-      img {
-        max-width : 70px;
-        top: 23%; 
-      }
+    height: 15vh;
+    span {
+      font-size: 50px;
+      text-shadow: 3px 4px 0px #000000;
+    }
+    img {
+      max-width: 70px;
+      top: 23%;
+    }
   }
 `;
 
@@ -85,11 +86,7 @@ const Sponsors1 = () => {
       <div className="flex justify-center flex-col w-screen h-96">
         <h1 className="text-3xl font-bold text-center mt-3 mb-6">Title Sponsor</h1>
         <div className="lg:ml-96 lg:mr-96 sm:ml-10 sm:mr-10 flex justify-center">
-          <img
-            src="fujifilm.svg"
-            alt=""
-            className="text-center pt-6 pb-0 h-60"
-          />
+          <img src="fujifilm.svg" alt="" className="text-center pt-6 pb-0 h-60" />
         </div>
       </div>
       {/* <hr className=" border border-black border-solid w-screen font-bold border-spacing-8" /> */}
@@ -97,11 +94,7 @@ const Sponsors1 = () => {
       <div className="flex justify-center flex-col w-screen h-96">
         <h1 className="text-3xl font-bold text-center mt-3">Powered By</h1>
         <div className="lg:ml-96 lg:mr-96 sm:ml-10 sm:mr-10 flex justify-center">
-          <img
-            src="Kalakari.svg"
-            alt=""
-            className="text-center h-80 pt-6 pb-0"
-          />
+          <img src="Kalakari.svg" alt="" className="text-center h-80 pt-6 pb-0" />
         </div>
       </div>
       <div className=" border-solid border-2 mt-6 w-screen border-black"></div>
@@ -109,21 +102,13 @@ const Sponsors1 = () => {
         <div class="flex-1  border-r border-black">
           <h1 className="text-3xl font-bold text-center mt-3">Co-Powered By</h1>
           <div className="object-contain flex justify-center align-middle">
-            <img
-              src="Lapcare.svg"
-              alt=""
-              className="text-center p-10 h-64"
-            />
+            <img src="Lapcare.svg" alt="" className="text-center p-10 h-64" />
           </div>
         </div>
         <div class=" flex-1  border-l border-black">
           <h1 className="text-3xl font-bold text-center mt-3">Co-Powered By</h1>
           <div className="object-contain flex justify-center align-middle">
-            <img
-              src="quillbot.svg"
-              alt=""
-              className="text-center p-10 h-56"
-            />
+            <img src="quillbot.svg" alt="" className="text-center p-10 h-56" />
           </div>
         </div>
       </div>
@@ -132,10 +117,8 @@ const Sponsors1 = () => {
         {SponsorData.map((sponsor) => {
           return (
             <Card
-              className="bg-[#685eda] "
               img={sponsor.img}
               title={sponsor.title}
-              name={sponsor.name}
             />
           );
         })}
@@ -143,10 +126,10 @@ const Sponsors1 = () => {
       <div className=" border-solid border-2 w-screen border-black"></div>
       <div className="flex w-screen flex-wrap justify-center p-4">
         <h1 className="text-3xl w-screen inline-block mb-6 font-bold text-center mt-3">
-          Previous Sponsers
+          Previous Sponsors
         </h1>
-        {SponsorData.map((sponsor) => {
-          return <img src={sponsor.img} className={Classes.img}/>;
+        {SponsorsData.map((sponsor) => {
+          return <img src={sponsor.img} className={Classes.img} />;
         })}
       </div>
       <Footer />
