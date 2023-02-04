06184e0a409a0c06dbf475994b97ef25b888ae62
AmitSharma512 <110725065+AmitSharma512@users.noreply.github.com>
2023-02-04 13:24:25 +0530

    Changes Sponsor , Faq , cart page. (#128)
    * Fixed events , faq , Teams , LandingPage
    
    * Added Error Page
    
    * Added mobile view of error page , register button.
    
    * Fixed error page
    
    * Updates previous sponsor section , sponsors design
    
    * Changes Sponsor , Faq , cart page.

--- src/components/pages/Sponsors/SponsorData1.js
+++ src/components/pages/Sponsors/SponsorData1.js
@@ -1,33 +1,33 @@
 const Sponsors = [
   {
-    title: 'Crypto Trading  Partner',
-    img: 'https://drive.google.com/uc?export=view&id=1fsc_Fq1gE-eGUuDfPCbApWUxqs3Wqy6U',
-    name: 'CoinDCX'
+    title: 'Design partner',
+    img: 'Huion.svg',
+    name: 'Huion',
   },
   {
-    title: 'Crypto Trading  Partner',
-    img: 'https://drive.google.com/uc?export=view&id=1fsc_Fq1gE-eGUuDfPCbApWUxqs3Wqy6U',
-    name: 'CoinDCX'
-  },
-  {
-    title: 'Crypto Trading  Partner',
-    img: 'https://drive.google.com/uc?export=view&id=1fsc_Fq1gE-eGUuDfPCbApWUxqs3Wqy6U',
-    name: 'CoinDCX'
-  },
-  {
-    title: 'Crypto Trading  Partner',
-    img: 'https://drive.google.com/uc?export=view&id=1fsc_Fq1gE-eGUuDfPCbApWUxqs3Wqy6U',
-    name: 'CoinDCX'
-  },
-  {
-    title: 'Crypto Trading  Partner',
-    img: 'https://drive.google.com/uc?export=view&id=1fsc_Fq1gE-eGUuDfPCbApWUxqs3Wqy6U',
-    name: 'CoinDCX'
-  },
-  {
-    title: 'Crypto Trading  Partner',
-    img: 'https://drive.google.com/uc?export=view&id=1fsc_Fq1gE-eGUuDfPCbApWUxqs3Wqy6U',
-    name: 'CoinDCX'
+    title: 'Bath and Body partner',
+    img: 'plum_bodylovin.webp',
+    name: 'Plum BodyLovin'
   }
+  // {
+  //   title: 'Crypto Trading  Partner',
+  //   img: 'https://drive.google.com/uc?export=view&id=1fsc_Fq1gE-eGUuDfPCbApWUxqs3Wqy6U',
+  //   name: 'CoinDCX'
+  // },
+  // {
+  //   title: 'Crypto Trading  Partner',
+  //   img: 'https://drive.google.com/uc?export=view&id=1fsc_Fq1gE-eGUuDfPCbApWUxqs3Wqy6U',
+  //   name: 'CoinDCX'
+  // },
+  // {
+  //   title: 'Crypto Trading  Partner',
+  //   img: 'https://drive.google.com/uc?export=view&id=1fsc_Fq1gE-eGUuDfPCbApWUxqs3Wqy6U',
+  //   name: 'CoinDCX'
+  // },
+  // {
+  //   title: 'Crypto Trading  Partner',
+  //   img: 'https://drive.google.com/uc?export=view&id=1fsc_Fq1gE-eGUuDfPCbApWUxqs3Wqy6U',
+  //   name: 'CoinDCX'
+  // }
 ];
 export default Sponsors;
