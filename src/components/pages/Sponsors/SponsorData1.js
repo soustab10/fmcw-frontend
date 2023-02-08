f50c76acd191cd2fee35e9ae7c0f934eb1722d88
Somesh Ranjan <somesh.ranjan.met20@itbhu.ac.in>
2023-02-08 12:39:38 +0530

    update sponsors data 4.0

--- src/components/pages/Sponsors/SponsorData1.js
+++ src/components/pages/Sponsors/SponsorData1.js
@@ -12,6 +12,12 @@ const Sponsors = [
     link: 'https://plumgoodness.com/pages/bodylovin'
   },
   {
+    title: 'National Media Partner',
+    img: process.env.REACT_APP_AWS_S3_URI + '/Amarujala.png',
+    name: 'Amar Ujala',
+    link: 'https://www.amarujala.com'
+  },
+  {
     title: 'Media Partner',
     img: process.env.REACT_APP_AWS_S3_URI + '/newshamster.jpg',
     name: 'News Hamster',
@@ -24,11 +30,23 @@ const Sponsors = [
     link: ''
   },
   {
+    title: 'Tea Partner',
+    img: process.env.REACT_APP_AWS_S3_URI + '/mbachaiwala.png',
+    name: 'MBA Chai Wala',
+    link: 'https://www.mbachaiwala.com'
+  },
+  {
     title: 'Book Reading Partner',
     img: process.env.REACT_APP_AWS_S3_URI + '/billionbook.png',
     name: 'Billionbooks',
     link: 'https://thebillionbooks.store'
-  }
+  },
+  {
+    title: 'Waffle Partner',
+    img: process.env.REACT_APP_AWS_S3_URI + '/Belgianwaffle.png',
+    name: 'News Hamster',
+    link: 'https://thebelgianwaffle.co/location/uttar-pradesh/varanasi/'
+  },
   // {
   //   title: 'Crypto Trading  Partner',
   //   img: 'https://drive.google.com/uc?export=view&id=1fsc_Fq1gE-eGUuDfPCbApWUxqs3Wqy6U',
